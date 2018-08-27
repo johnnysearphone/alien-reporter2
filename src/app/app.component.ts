7be741cd26076b700bb672a5682b0b46e316537c
johnnysearphone <keithprussell@gmail.com>
2018-08-27 19:06:52 +0100

    see summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alien-reporter';
+}
+*/
+
+import { Component, OnInit } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import * as data from './example.json';
+@Component({
+  selector: 'app-simple-http',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  data: any;
+  loading: boolean;
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+  }
+
+
+  makeRequest(): void {
+    this.loading = true;
+    this.http.request('http://www.mocky.io/v2/59f7760a2f0000ab1d55864e')
+    .subscribe((res: Response) => {
+      this.data = res.json();
+      console.log(this.data);
+      this.loading = false;
+    });
+  }
 }
